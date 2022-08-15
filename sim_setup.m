c6aa80430ec8b102809d87c8487b64c7759b0548
Alina Kuliesh <alina.kuliesh@gmail.com>
2022-08-15 17:42:23 +0200

    40 elements

--- sim_setup.m
+++ sim_setup.m
@@ -10,7 +10,7 @@ elseif run_param.maroilles_gpu
     addpath(genpath("~/k-wave-toolbox-version-1.3"));
     addpath(genpath("~/Mesh_voxelisation"));
     run_param.DATA_CAST = 'gpuArray-single';
-    run_param.DATA_PATH = '~/MIS_opt_fullRT/job_1';
+    run_param.DATA_PATH = '~/MIS_opt_fullRT/job';
     run_param.DEVICE_NUM = 1; % select gpu device [0 1 2] 
 elseif run_param.dhpc
     addpath(genpath("/scratch/akuliesh1/Matlab_Toolboxes"));
